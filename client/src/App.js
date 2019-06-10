4126b8a301a228b8d49b100620be2432ef99e18e
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-10 10:41:52 +0000

    feature: Route for '/movies/:id' that loads the Movie component

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ export default class App extends Component {
         <div>
           <SavedList list={this.state.savedList} />
           <Route path='/' component={MovieList} />
+          <Route path='/movies/:id' component={Movie} />
         </div>
       </Router>
     );
