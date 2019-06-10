ad359bb639a6d49f71d92d9f68e86de301ae6930
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-10 11:42:38 +0000

    refactor: Make code DRY Replace movie card elements with MovieCard component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class Movie extends Component {
   constructor(props) {
@@ -45,22 +46,12 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard
+          title={title}
+          director={director}
+          metascore={metascore}
+          stars={stars}
+        />
         <div className="save-button">Save</div>
       </div>
     );
