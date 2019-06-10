66632015a583ccc5ff16fb25652caa30e2937c39
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-10 10:36:40 +0000

    feature: Import and wrap app with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
@@ -20,10 +21,12 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
+      <Router>
+        <div>
+          <SavedList list={this.state.savedList} />
+          <div>Replace this Div with your Routes</div>
+        </div>
+      </Router>
     );
   }
 }
